0e9052e455118b5860f2dbb61ff53a07811e131d
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-16 11:19:52 -0600

    Create createdb.sql

---
+++ Analyzing NYC Public School Test Result Scores/createdb.sql
@@ -0,0 +1,11 @@
+DROP TABLE schools; 
+
+CREATE TABLE schools(
+  school_name VARCHAR(100) PRIMARY KEY, 
+  borough VARCHAR(100), 
+  building_code VARCHAR(10),
+  average_math INT,
+  average_reading INT,
+  average_writing INT, 
+  percent_tested FLOAT
+); 
